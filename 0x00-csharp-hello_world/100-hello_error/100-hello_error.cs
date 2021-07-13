8490e93425781d45b065be1c51b6e588a78c02d4
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-12 19:41:06 -0500

    [NEW] - Print string with exit status 1

--- 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
+++ 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -6,7 +6,7 @@ namespace _100_hello_error
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("and that piece of art is useful - Dora Korpar, 2015-10-19");
+            Console.Error.WriteLine("and that piece of art is useful - Dora Korpar, 2015-10-19");
             Environment.ExitCode = 1;
         }
     }

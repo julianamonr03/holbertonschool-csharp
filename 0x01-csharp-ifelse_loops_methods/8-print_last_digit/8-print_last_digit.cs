d76d7e02fb76c7c46d22dbc659a85c542bf854b1
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-13 22:34:59 -0500

    Print last digit

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+    class Number
+    {
+        public static int PrintLastDigit(int number)
+        {
+            int last = 0;
+
+            if (number < 0)
+            {
+                last = ((number % 10) * - 1);
+            }
+            else
+            {
+                last = number % 10;
+            }
+            Console.Write(last);
+            return (last);
+        }
+    }

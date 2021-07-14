8396c84587993ffc46bb5b56a6ee2fa6cba02885
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-13 23:22:20 -0500

    [NEW] - FizzBuzz

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 1; number <= 100; number++)
+            {
+                if (number % 15 == 0)
+                {
+                    Console.Write("FizzBuzz ");
+                }
+                else if (number == 100)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (number % 3 == 0)
+                {
+                    Console.Write("Fizz ");
+                }
+                else if (number % 5 == 0)
+                {
+                    Console.Write("Buzz ");
+                }
+                else
+                {
+                    Console.Write("{0} ", number);
+                }
+            }
+        }
+    }
+}

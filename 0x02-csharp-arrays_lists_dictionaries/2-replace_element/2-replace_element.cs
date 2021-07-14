9f4fbdd54b8d44908c0364e32955fdb48a7d0c79
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-14 18:18:41 -0500

    [UPDATE] - Change condition of index out of rNGE

--- 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -4,7 +4,7 @@
     {
         public static int[] ReplaceElement(int[] array, int index, int n)
         {
-            if (index < 0 || index > array.Length)
+            if (index < 0 || index >= array.Length)
             {
                 Console.WriteLine("Index out of range");
                 return (array);

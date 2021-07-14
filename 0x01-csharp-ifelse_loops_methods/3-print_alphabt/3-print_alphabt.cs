7e28eed38ce4747eb444d17e67cda4f7746abd1c
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-13 20:20:44 -0500

    [NEW] - Print alph without e and q letters

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char alph = 'a'; alph <= 'z'; alph++)
+            {
+                if (alph != 'e' && alph != 'q')
+                {
+                    Console.Write(alph);
+                }
+            }
+        }
+    }
+}

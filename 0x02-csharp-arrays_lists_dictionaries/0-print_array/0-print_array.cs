5a6b15bc913d87b1b5b1abdeb1d8ea87eb3d9a77
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-14 18:02:03 -0500

    [NEW] - Print array of integers and return a new array

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+    class Array
+    {
+        public static int[] CreatePrint(int size)
+        {
+            if (size == 0)
+            {
+                Console.WriteLine("");
+                return new int[0];
+            }
+            else if (size < 0)
+            {
+                Console.WriteLine("Size cannot be negative");
+                return (null);
+            }
+
+            int[] new_array = new int[size];
+
+            for (int number = 0; number < size; number++)
+            {
+                new_array[number] = number;
+            }
+            Console.WriteLine(string.Join(" ", new_array));
+                return (new_array);
+        }
+    }

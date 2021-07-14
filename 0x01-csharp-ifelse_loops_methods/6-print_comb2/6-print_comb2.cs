1b9082766f9d51129c43fa1e481d799b76597e17
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-13 22:04:56 -0500

    [NEW] - print unique numbers

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int first = 0; first <= 8; first++)
+            {
+                for (int j = 0; j <= 9; j++)
+                {
+                    if (first < j)
+                    {
+                        if (first != 8 || j != 9)
+                        {
+                            Console.Write("{0}{1}, ", first, j);
+                        }
+                        else
+                        {
+                            Console.WriteLine("{0}{1}", first, j);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

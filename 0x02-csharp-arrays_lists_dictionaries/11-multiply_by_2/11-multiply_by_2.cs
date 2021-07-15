049444f3692c1e438e08cac8860179d8a94e9fb5
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-14 21:25:31 -0500

    [NEW] - Double each value in Dic

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class Dictionary
+    {
+        public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+        {
+            Dictionary<string, int> new_double = new Dictionary<string, int>();
+
+            foreach (var each in myDict)
+            {
+                new_double[each.Key] = each.Value * 2;
+            }
+            return (new_double);
+        }
+    }
+

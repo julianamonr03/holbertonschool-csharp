4ea35e149c4f9beaa6122bd03c055444bb2c8553
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-14 21:34:43 -0500

    [NEW] - Print sorted Dic in Keys

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+    class Dictionary
+    {
+        public static void PrintSorted(Dictionary<string, string> myDict)
+        {
+            foreach (var item in myDict.OrderBy(x => x.Key))
+            {
+                Console.WriteLine("{0}: {1}", item.Key, item.Value);
+            }
+        }
+    }

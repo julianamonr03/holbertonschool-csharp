b5e71cea968c0257a99dea6b6eae9ecab8761182
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-14 19:54:25 -0500

    [NEW] - New list with even

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class List
+    {
+        public static List<bool> DivisibleBy2(List<int> myList)
+        {
+            List<bool> new_rtn = new List<bool>();
+
+            foreach (int each in myList)
+            {
+                new_rtn.Add(each % 2 == 0 ? true : false);
+            }
+            return (new_rtn);
+        }
+    }

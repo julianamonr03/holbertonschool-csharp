e4440850e5916b573e39a6d1fe963b6d2eadbabc
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-14 19:32:57 -0500

    [NEW] - creates and print list

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static List<int> CreatePrint(int size)
+        {
+            if (size < 0)
+            {
+                Console.WriteLine("Size cannot be negative");
+                return (null);
+            }
+
+            List<int> new_list = new List<int>(size);
+
+            for (int i = 0; i < size; i++)
+            {
+                new_list.Add(i);
+            }
+            Console.WriteLine(string.Join(" ", new_list));
+            return (new_list);
+        }
+    }

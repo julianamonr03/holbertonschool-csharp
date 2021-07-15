d15c732273f2b233cb879367743a87d3f4498a35
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-14 19:41:22 -0500

    [NEW] - Return max integer

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static int MaxInteger(List<int> myList)
+        {
+            if (myList == null)
+            {
+                Console.WriteLine("List is empty");
+                return (-1);
+            }
+            myList.Sort();
+            int max = myList[myList.Count - 1];
+            return (max);
+        }
+    }

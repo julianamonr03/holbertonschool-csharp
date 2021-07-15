cced25888f52d3f22b1c309a41a3e8ec7ccd348d
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-14 23:18:09 -0500

    [NEW] - Get square of all values in matrix

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+    class Matrix
+    {
+        public static int[,] Square(int[,] myMatrix)
+        {
+            int row = myMatrix.GetLength(0);
+            int col = myMatrix.GetLength(1);
+
+            int[,] newOne = new int[row, col];
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    newOne[i, j] = myMatrix[i, j] * myMatrix[i, j];
+                }
+            }
+            return (newOne);
+        }
+    }

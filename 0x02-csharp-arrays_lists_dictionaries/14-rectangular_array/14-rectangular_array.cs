065b44ef6ca3769d241912a62b1affb4cf0433f1
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-14 23:09:21 -0500

    [NEW] - Print a rectangular array with 2D array

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array_D = new int[5, 5];
+
+            array_D[2, 2] = 1;
+
+            for (int i = 0; i < array_D.GetLength(0); i++)
+            {
+                for (int j = 0; j < array_D.GetLength(1); j++)
+                {
+                    Console.Write("{0} ", array_D[i,j]);
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}

c0782a7b2f97f70454e599031e95dce35dd27c2f
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-14 22:43:57 -0500

    [NEW] - Find the highest score of a dictionary

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+    class Dictionary
+    {
+        public static string BestScore(Dictionary<string, int> myList)
+        {
+            if (myList.Count == 0)
+            {
+                return ("None");
+            }
+
+            // c => comparision value, s => selected element
+            var max = myList.Aggregate((c, s) => c.Value > s.Value ? c : s).Key;
+            // loops over each element and comparison function that returns the bigger value
+            return (max);
+        }
+    }

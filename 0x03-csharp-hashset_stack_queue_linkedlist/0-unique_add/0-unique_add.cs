3bb346d59e7148fb6204704c0869aa3542ead22b
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-15 20:41:59 -0500

    [NEW] - Return sum of unique items in a list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+    class List
+    {
+        public static int Sum(List<int> myList)
+        {
+            int total = 0;
+
+            HashSet<int> unique = new HashSet<int>(myList);
+
+            foreach (int sum in unique)
+            {
+                total += sum;
+            }
+
+            return (total);
+        }
+    }

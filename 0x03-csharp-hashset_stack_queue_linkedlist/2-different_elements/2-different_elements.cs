c9f4284e2425ef4ca9b97b1c458b0ca4b05344e6
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-15 21:30:53 -0500

    [NEW] - Common elements in two lists return in new sorted list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static List<int> DifferentElements(List<int> list1, List<int> list2)
+        {
+            List<int> tmp = new List<int>();
+            List<int> common = new List<int>();
+
+            tmp = list1.FindAll(x => !list2.Contains(x));
+            common = list2.FindAll(x => !list1.Contains(x));
+
+            common.AddRange(tmp);
+            common.Sort();
+            return (common);
+        }
+    }
+

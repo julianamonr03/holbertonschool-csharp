6099d9548b386348532cde98e5567217e388be6a
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-15 21:14:35 -0500

    [NEW] - Return new list with common elements

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static List<int> CommonElements(List<int> list1, List<int> list2)
+        {
+            List<int> common_sorted = new List<int>();
+
+            common_sorted = list1.FindAll(x => list2.Contains(x));
+            // Retrieves elms that match the conditions define
+            // and returns a bool if elm is in the list
+            return (common_sorted);
+        }
+    }

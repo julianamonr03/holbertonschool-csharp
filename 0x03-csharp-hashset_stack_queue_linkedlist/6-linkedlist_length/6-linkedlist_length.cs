1b7b471cd34a6a61ddd4319542b69f24320d8d21
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-18 21:26:06 -0500

    [NEW] - Return number of elements in a linkedlist without using count

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+    class LList
+    {
+        public static int Length(LinkedList<int> myLList)
+        {
+            int number = 0;
+
+            foreach (int each in myLList)
+            {
+                number += 1;
+            }
+            return (number);
+        }
+    }
+

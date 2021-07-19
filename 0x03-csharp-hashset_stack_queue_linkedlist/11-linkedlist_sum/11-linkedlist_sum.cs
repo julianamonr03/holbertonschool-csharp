a7cebf56af3134aaa4386e0270b48073177f96b3
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-18 22:43:22 -0500

    [NEW] - Returns the sum of all elements in list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+    class LList
+    {
+        public static int Sum(LinkedList<int> myLList)
+        {
+            int sum = 0;
+            foreach (var num in myLList)
+            {
+                sum += num;
+            }
+            return (sum);
+        }
+    }

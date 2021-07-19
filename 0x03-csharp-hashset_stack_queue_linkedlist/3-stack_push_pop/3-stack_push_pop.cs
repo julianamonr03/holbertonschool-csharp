2d8d909718db7b3cd86acb9ecd03529cb3e63953
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-18 20:09:01 -0500

    [NEW] - Stack method Top, counting, search, remove and add

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+    class MyStack
+    {
+        public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+        {
+            Console.WriteLine("Number of items: {0}", aStack.Count);
+
+            if (aStack.Count <= 0)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+
+            bool answer = aStack.Contains(search);
+
+            if (aStack.Contains(search))
+            {
+                Console.WriteLine("Stack contains \"{0}\": {1}", search, answer);
+
+                while (aStack.Contains(search))
+                {
+                    aStack.Pop();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Stack contains {0}: {1}", search, answer);
+            }
+            aStack.Push(newItem);
+            return (aStack);
+        }
+    }
+

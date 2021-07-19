cb15c327e350a02bc21855693e97a604f92d1056
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-18 20:21:07 -0500

    [NEW] - Manage queues adding and removing

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class MyQueue
+    {
+        public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+        {
+            Console.WriteLine("Number of items: {0}", aQueue.Count);
+
+            Console.WriteLine("First item: {0}", aQueue.Peek());
+
+            if (aQueue.Count <= 0)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+
+            aQueue.Enqueue(newItem);
+            bool answer = aQueue.Contains(search);
+
+            if (aQueue.Contains(search))
+            {
+                Console.WriteLine("Queue contains \"{0}\": {1}", search, answer);
+
+                while (aQueue.Contains(search))
+                {
+                    aQueue.Dequeue();
+                }
+            }
+            return (aQueue);
+        }
+    }

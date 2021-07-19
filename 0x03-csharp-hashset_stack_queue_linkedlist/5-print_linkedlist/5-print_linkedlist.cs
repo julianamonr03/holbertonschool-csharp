a75684431991619dd074f1710b6ea119cbaa93af
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-18 21:02:11 -0500

    [NEW] - print linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+    class LList
+    {
+        public static LinkedList<int> CreatePrint(int size)
+        {
+            LinkedList<int> new_linked = new LinkedList<int>();
+
+            if (size <= 0)
+            {
+                return (new_linked);
+            }
+
+            for (int i = 0; i < size ; i++)
+            {
+                new_linked.AddFirst(i);
+                Console.WriteLine(i);
+            }
+            return (new_linked);
+        }
+    }

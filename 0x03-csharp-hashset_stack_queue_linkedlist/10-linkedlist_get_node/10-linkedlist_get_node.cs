90fce3bcb668c534dc8d0b09a8d9630558b7910e
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-18 22:37:04 -0500

    [NEW] - Get element at  nth node of a LinkedList.

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+    class LList
+    {
+        public static int GetNode(LinkedList<int> myLList, int n)
+        {
+            if (myLList.Contains(n) == true)
+            {
+                return (0);
+            }
+            return (myLList.ElementAt(n));
+        }
+    }

2cbec5780cb1889b83d27645e25cbdf8b3c25691
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-18 21:43:42 -0500

    [NEW] - Find a value and return its position

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class LList
+    {
+        public static int FindNode(LinkedList<int> myLList, int value)
+        {
+            int position = 0;
+
+            foreach (int each in myLList)
+            {
+                if (each == value)
+                {
+                    return (position);
+                }
+                position++; // increment in every loop == index
+            }
+            return (-1); // if value not in the list return -1
+        }
+    }

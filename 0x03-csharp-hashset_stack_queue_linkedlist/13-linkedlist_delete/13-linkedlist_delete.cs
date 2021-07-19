0dca9b77e167c57e6e145d368a7c77c89f43022d
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-18 23:08:23 -0500

    [NEW] - Remove a node at given position

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+    class LList
+    {
+        public static void Delete(LinkedList<int> myLList, int index)
+        {
+            LinkedListNode<int> node = myLList.First; //node to remove
+
+            int search = 0;
+
+            while (node != null)
+            {
+                if (search == index)
+                {
+                    myLList.Remove(node);
+                }
+                node = node.Next; //If not matches with index pass to next position
+                search++; // increment index for search
+            }
+        }
+    }

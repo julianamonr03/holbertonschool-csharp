c3edb17e450fb86f79cd7b740c2a9d443fe316d6
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-26 13:45:01 -0500

    [NEW] - Prints n elements of a list and return number of elements printed

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static int SafePrint(List<int> myList, int n)
+        {
+            int number_elements = 0;
+            try
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine(myList[i]);
+                    number_elements++; // Increment each loop for number of elements printed
+                }
+            }
+            catch (Exception)
+            {
+                return(number_elements);
+            }
+            return (number_elements);
+        }
+    }
+

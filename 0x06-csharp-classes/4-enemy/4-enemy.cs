800a8a597e41033ba21d43f87dcffb5f54614a8a
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-29 11:09:04 -0500

    [NEW] - Add property and private field

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary> Public class Zombie with Enemies. </summary>
+    public class Zombie
+    {
+        /// <summary> Private field health </summary>
+        private int health;
+
+        /// <summary> Public constructor </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summary> Public constructor </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+        /// <summary> Private field name </summary>
+        private string name = "(No name)";
+
+        /// <summary> Public Constructor name </summary>
+        public string Name
+        {
+            get {return name;}
+            set {name = value;}
+        }
+
+        /// <summary> Public constructor Health </summary>
+        public int GetHealth()
+        {
+            return (health);
+        }
+    }
+}

882802984f8ef7f44a9f7bc2f951fd11352d6a3e
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-29 10:02:05 -0500

    [NEW] - Constructor an field

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary> Public class Zombie with Enemies. </summary>
+    public class Zombie
+    {
+        /// <summary> Public field health </summary>
+        public int health;
+
+        /// <summary> Public constructor </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
+

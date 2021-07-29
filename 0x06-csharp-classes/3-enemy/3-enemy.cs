2bee695c18fe61f05daf5083410d4275eb54ca26
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-29 10:48:39 -0500

    [ZOMBIE CLASS] - with private field

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary> Public class Zombie with Enemies. </summary>
+    public class Zombie
+    {
+        /// <summary> Public field health </summary>
+        private int health;
+
+        /// <summary> Public constructor </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summary> Public constructor </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+
+        /// <summary> Public constructor Health </summary>
+        public int GetHealth()
+        {
+            return (health);
+        }
+    }
+}

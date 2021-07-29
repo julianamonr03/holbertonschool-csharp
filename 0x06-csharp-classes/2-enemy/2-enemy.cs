890a258d232b9e67d61ae42e9276243186b12a61
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-29 10:31:40 -0500

    [NEW] - Exception and health equal to value

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary> Public class Zombie with Enemies. </summary>
+    public class Zombie
+    {
+        /// <summary> Public field health </summary>
+        public int health;
+
+        /// <summary> Public constructor </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summary> Public constructor </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+    }
+}

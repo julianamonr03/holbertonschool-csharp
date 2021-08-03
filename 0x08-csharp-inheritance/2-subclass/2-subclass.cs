5bba0fba3e32485a84fdf9ff3449563495d41c0c
julianamonr03 <julianamonroy03@hotmail.com>
2021-08-03 16:03:31 -0500

    [NEW] - Fcuntion that returns if a class is an instance from a specified class

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+    /// <summary> Public class Obj </summary>
+    class Obj
+    {
+         /// <summary> If class is a Subclass from BaseType </summary>
+        public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+        {
+            return (derivedType.IsSubclassOf(baseType));
+        }
+    }

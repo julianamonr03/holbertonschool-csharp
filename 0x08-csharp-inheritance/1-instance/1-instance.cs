d05d046f663886e5b27ac8629f5204f2d7178fe5
julianamonr03 <julianamonroy03@hotmail.com>
2021-08-03 11:32:54 -0500

    [NEW] - Return true if is an instance of Array

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+    /// <summary> Public class Obj </summary>
+    class Obj
+    {
+        /// <summary> Public class Obj </summary>
+        public static bool IsInstanceOfArray(object obj)
+        {
+            return (obj is Array);
+        }
+    }

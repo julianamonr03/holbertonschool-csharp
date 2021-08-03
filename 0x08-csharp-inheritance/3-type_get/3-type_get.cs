3357289ad7696abc7c509675fd7ace9537707d82
julianamonr03 <julianamonroy03@hotmail.com>
2021-08-03 17:04:57 -0500

    [UPDATE] - prints the names of the available properties and methods of an object

--- 0x08-csharp-inheritance/3-type_get/3-type_get.cs
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -2,26 +2,25 @@
 using System.Collections.Generic;
 using System.Reflection;
 
-
     /// <summary> Public class Obj </summary>
     class Obj
     {
         /// <summary> Public Static Obj </summary>
         public static void Print(object myObj)
         {
-            Type new_obj = myObj.GetType().GetTypeInfo(); // Where obj is object whose properties you need.
-            Console.WriteLine("{0} Properties: ", new_obj.Name);
+            Type new_obj = myObj.GetType().GetTypeInfo(); // Where new_obj is object whose properties and methods you need.
+            Console.WriteLine("{0} Properties:", new_obj.Name);
 
             foreach (PropertyInfo p in new_obj.GetProperties())
             {
                 Console.WriteLine("{0}", p.Name);
             }
 
-            Console.WriteLine("{0} Methods: ", new_obj.Name);
+            Console.WriteLine("{0} Methods:", new_obj.Name);
 
-            foreach (MethodInfo p in new_obj.GetMethods())
+            foreach (MethodInfo m in new_obj.GetMethods())
             {
-                Console.WriteLine("{0}", p.Name);
+                Console.WriteLine("{0}", m.Name);
             }
         }
     }

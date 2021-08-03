c073ceb38824d8c1087996e675653b1fe6d7bdea
julianamonr03 <julianamonroy03@hotmail.com>
2021-08-03 17:28:21 -0500

    [NEW] - Empty class that inherith from a base class

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+    /// <summary> New empty class Animal </summary>
+    class Animal
+    {
+
+    }
+    /// <summary> New class Dog inherits from Animal </summary>
+    class Dog: Animal
+    {
+
+    }

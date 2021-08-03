d5ec6f1f41d85a6a01a0b4e115b465275b2fa310
julianamonr03 <julianamonroy03@hotmail.com>
2021-08-03 17:33:50 -0500

    [NEW] - Implementing exception shape

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+    /// <summary> New class Shape </summary>
+    class Shape
+    {
+        /// <summary> Public Area with Exception </summary>
+        public virtual int Area()
+        {
+            throw new NotImplementedException("Area() is not implemented");
+        }
+    }

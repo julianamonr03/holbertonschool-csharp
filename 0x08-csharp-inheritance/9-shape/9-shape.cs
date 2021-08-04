30dcca0c5e02a9bdc82b2ac7ac26913c18eea974
julianamonr03 <julianamonroy03@hotmail.com>
2021-08-03 22:19:44 -0500

    [NEW] - Override string for Square

---
+++ 0x08-csharp-inheritance/9-shape/9-shape.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+    /// <summary> New class Shape </summary>
+    class Shape
+    {
+        /// <summary> Public Area with Exception </summary>
+        public virtual int Area()
+        {
+            throw new NotImplementedException("Area() is not implemented");
+        }
+    }
+
+    /// <summary> Public Area with Exception </summary>
+    class Rectangle: Shape
+    {
+        private int width;
+        private int height;
+
+        /// <summary> Public Area with Exception </summary>
+        public int Width
+        {
+            get {return width;}
+
+            set {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Width must be greater than or equal to 0");
+                }
+                width  = value;
+            }
+
+        }
+
+        /// <summary> Public Area with Exception </summary>
+        public int Height
+        {
+            get {return height;}
+
+            set {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Height must be greater than or equal to 0");
+                }
+                height = value;
+            }
+        }
+
+        // <summary> Public Area for rectangle </summary>
+        public new int Area()
+        {
+            return (width * height);
+        }
+
+        // <summary> Public Override </summary>
+        public override string ToString()
+        {
+            return string.Format("[Rectangle] {0} / {1}", width, height);
+        }
+    }
+
+    // <summary> Class Square that inherits from Rectangle </summary>
+    class Square: Rectangle
+    {
+        private int size;
+
+        public int Size
+        {
+            get {return size;}
+
+            set {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Size must be greater than or equal to 0");
+                }
+                size = value;
+                Height = value;
+                Width = value;
+            }
+        }
+        public override string ToString()
+        {
+            return (string.Format("[Square] {0} / {1}", Width, Height));
+        }
+    }
+

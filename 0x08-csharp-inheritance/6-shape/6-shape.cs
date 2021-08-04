1bf3018e51c6ddeaf65213005f809e2b49403d78
julianamonr03 <julianamonroy03@hotmail.com>
2021-08-03 21:08:36 -0500

    [NEW] - Inheritance shape class

---
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+    /// <summary> New class Shape </summary>
+    class Shape
+    {
+        /// <summary> Public Area with Exception </summary>
+        public virtual int Area()
+        {
+            throw new NotImplementedException("Area() is not implemented");
+        }
+    }
+
+    /// <summary> Public Area with Exception </summary>
+    class Rectangle: Shape
+    {
+        private int width;
+        private int height;
+
+        /// <summary> Public Area with Exception </summary>
+        public int Width
+        {
+            get {return width;}
+
+            set {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Width must be greater than or equal to 0");
+                }
+                width  = value;
+            }
+
+        }
+
+        /// <summary> Public Area with Exception </summary>
+        public int Height
+        {
+            get {return height;}
+
+            set {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Height must be greater than or equal to 0");
+                }
+                height = value;
+            }
+        }
+    }

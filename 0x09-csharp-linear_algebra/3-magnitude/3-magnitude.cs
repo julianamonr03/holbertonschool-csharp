ff992e6d349a3aa6d8b58c4a1c3f6a85d7ae5282
julianamonr03 <julianamonroy03@hotmail.com>
2021-08-19 12:37:45 -0500

    Calculat ethe magnitude of a given vector

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+    // <sumary> Main class  </sumary>
+    class VectorMath
+    {
+        // <sumary> Calculates the lenght of a given vector </sumary>
+        public static double Magnitude(double[] vector)
+        {
+            if (vector.Length == 2)
+            {
+                return (Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2)), 2));
+            }
+
+            if (vector.Length == 3)
+            {
+                return (Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)), 2));
+            }
+
+            else
+            {
+                return (-1);
+            }
+        }
+    }

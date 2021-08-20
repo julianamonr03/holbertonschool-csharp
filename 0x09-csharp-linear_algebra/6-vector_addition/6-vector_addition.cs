2d1e1895b41dd916741232fe7142c0843460ed54
julianamonr03 <julianamonroy03@hotmail.com>
2021-08-19 22:20:47 -0500

    Calculates the sum of two vectors of 2D AND 3D

---
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+    // <sumary> Main Class  </sumary>
+    class VectorMath
+    {
+        // <sumary> Calculates the sum of two vectors </sumary>
+        public static double[] Add(double[] vector1, double[] vector2)
+        {
+            if (vector1.Length == 2 && vector2.Length == 2)
+            {
+                double[] sum = {0, 0};
+                sum[0] = vector1[0] + vector2[0];
+                sum[1] = vector1[1] + vector2[1];
+                return (sum);
+            }
+
+            else if (vector1.Length == 3 && vector2.Length == 3)
+            {
+                double[] sum = {0, 0, 0};
+                sum[0] = vector1[0] + vector2[0];
+                sum[1] = vector1[1] + vector2[1];
+                sum[2] = vector1[2] + vector2[2];
+                return (sum);
+            }
+
+            else
+            {
+                return (new double[] {-1});
+            }
+        }
+    }

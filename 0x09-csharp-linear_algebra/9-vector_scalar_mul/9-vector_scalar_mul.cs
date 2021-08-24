e6b5f97f79af6070f0b3134180a00155aee2894e
julianamonr03 <julianamonroy03@hotmail.com>
2021-08-24 16:02:21 -0500

    Function for Scalar vector multiplication

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+    // <sumary> Main Class  </sumary>
+    class VectorMath
+    {
+        // <sumary> Scalar vector multiplication </sumary>
+        public static double[] Multiply(double[] vector, double scalar)
+        {
+            if (vector.Length == 2)
+            {
+                double[] mul = {0, 0};
+                mul[0] = vector[0] * scalar;
+                mul[1] = vector[1] * scalar;
+                return (mul);
+            }
+
+            if (vector.Length == 3)
+            {
+                double[] mul = {0, 0, 0};
+                mul[0] = vector[0] * scalar;
+                mul[1] = vector[1] * scalar;
+                mul[2] = vector[2] * scalar;
+                return (mul);
+            }
+
+            else
+            {
+                return (new double[] {-1});
+            }
+        }
+    }

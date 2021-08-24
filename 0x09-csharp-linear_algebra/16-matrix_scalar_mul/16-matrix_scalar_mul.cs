a507af860a475e4a07212586bae9a8949b71177f
julianamonr03 <julianamonroy03@hotmail.com>
2021-08-24 18:57:01 -0500

    Method for matrix scalar multiplication

---
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+    // <sumary> Main Class  </sumary>
+    class MatrixMath
+    {
+        // <sumary> Method for matrix scalar multiplication  </sumary>
+        public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+        {
+            double[,] error_matrix = { { -1 } };
+
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+
+            if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2 || matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
+            {
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        matrix[i, j] = matrix[i, j] * scalar;
+                    }
+                }
+
+                return (matrix);
+            }
+
+            else
+            {
+                return (error_matrix);
+            }
+        }
+    }
